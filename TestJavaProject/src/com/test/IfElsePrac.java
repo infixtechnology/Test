7fb43a91f16f1a771950bf9c9516b67d73a9e964
Reaz Patwary <infixtechnology@gmail.com>
2014-04-05 13:09:18 -0400

    class work

---
+++ TestJavaProject/src/com/test/IfElsePrac.java
@@ -0,0 +1,23 @@
+package com.test;
+
+public class IfElsePrac {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i =9;
+		if(i%2==0){
+			System.out.println("the number is even");
+			
+		}
+		else if(i%2==1){
+			System.out.println("the number is odd");
+		}
+		
+
+	}
+
+}
